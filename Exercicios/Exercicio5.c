db95f5b443657e3265d02f5fa3be020bd1e278f0
Fernando <142359611+Feernando19@users.noreply.github.com>
2023-09-29 19:34:52 -0300

    Exercicio5.c

--- Exercicios/Exercicio5.c
+++ Exercicios/Exercicio5.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
-
 int main() {
-    int a = 0, b = 0;
+
+    int A, B;
+
+    scanf("%d", &A);
     
-    scanf("%d %d", &a, &b);
-    
-    if (a > b) {
-         printf("%d eh maior que %d\n ", a ,b);
-        if (a % b == 0) {
-            printf("%d eh multiplo de %d\n",a, b);
-        } else {
-             printf("%d nao eh multiplo de %d\n",a, b);
-        }
-    } else if (a < b)
-   
-     {
-         printf("%d eh maior que %d\n ", b ,a);
-        if (b % a == 0) {
-            printf("%d eh multiplo de %d\n",b, a);
-        } else {
-           printf("%d nao eh multiplo de %d\n",b, a);
-        }
-       
-    }else{
+    scanf("%d", &B);
+
+    if (A == B) {
         printf("Os valores lidos sao iguais\n");
+    } else {
+        
+        int maior, menor;
+        if (A > B) {
+            maior = A;
+            menor = B;
+        } else {
+            maior = B;
+            menor = A;
+        }
+
+        printf("%d eh maior que %d\n", maior, menor);
+        
+        if (menor != 0 && maior % menor == 0) {
+            printf("%d eh multiplo de %d\n", maior, menor);
+        } else {
+            printf("%d nao eh multiplo de %d\n", maior, menor);
+        }
     }
+
     return 0;
+    
 }
